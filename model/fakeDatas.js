c35515447879658e1d9932c7462f178485525158
JosephCaillet <josephcaillet@gmail.com>
2018-03-15 15:18:32 +0100

    change timestamp from ms to s, create base db

--- model/fakeDatas.js
+++ model/fakeDatas.js
@@ -3,7 +3,7 @@ let donnees = require('../model/donnees')
 module.exports.fakePatient = {
 	"name": "Bob",
 	"surname": "Léponge",
-	"birth": 719664630,
+	"birth": 719664630000,
 	"gender": "M"
 }
 
@@ -11,20 +11,20 @@ let fakeDonnees = {}
 for( let valueType in donnees.VALUE_TYPES ) {
 	fakeDonnees[donnees.VALUE_TYPES[valueType]] = [
 		{
-			time:1521043765,
+			time:1521043765000,
 			value: 36
 		},
 		{
-			time:1521053765,
-			value: 36
+			time:1521053765000,
+			value: 40
 		},
 		{
-			time:1521063765,
-			value: 36
+			time:1521063765000,
+			value: 37
 		},
 		{
-			time:1521073765,
-			value: 36
+			time:1521073765000,
+			value: 45
 		}
 	]
 }
--- server.js
+++ server.js
@@ -17,6 +17,7 @@ server.listen(8081)
 let wss = new WebSocket.Server({ port: 8080 })
 wss.on('connection', ws => {
 	ws.on('message', msg => {
+		console.log("---")
 		let json = readEventMessage(msg)
 		if(json == undefined) {
 			console.error("Invalid message: ", msg)
